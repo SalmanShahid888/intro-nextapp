1c8614f011c5e9b1d52ed165283f12a9b200feca
salman8896 <salmanshahid8896@gmail.com>
2023-05-30 13:12:57 +0500

    Setting up background and tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-
-
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 export default async function Home() {
-  return <div>hello</div>
+  return (
+    <div className={styles.home}>
+      <div>
+        <div>
+          <h1>Hi, My Name is Salman Shahid!</h1>
+        </div>
+        <div>
+          <Link href="/blog">Check Out My Blog</Link>
+        </div>
+        <div>
+          <Link href="/contact">Contact me</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
